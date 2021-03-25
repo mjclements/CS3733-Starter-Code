ac3842ad69a37e1dd7af6f5d466b97601a2e74d3
rluu1 <rmluu@wpi.edu>
2021-03-25 15:28:55 -0400

    Ryan Luu

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -12,6 +12,6 @@ public class Main {
     System.out.println("Team Member Names Below:");
     System.out.println("Michael Clements");
     System.out.println("Team Coach: Michael Clements");
-
+    System.out.println("Ryan Luu");
   }
 }
