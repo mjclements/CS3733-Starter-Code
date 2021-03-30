83a08ad62dc5a3df19e5acb2252c150a08b348cb
Freud <foulon@wpi.edu>
2021-03-29 22:14:10 -0400

    branched created

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -14,9 +14,11 @@ public class Main {
     System.out.println("Team Coach: Michael Clements");
     System.out.println("Anthony LoPresti");
     System.out.println("Conor McDonough");
+    System.out.println("Freud Oulon");
   }
 
   public void printBranchAL() {
     System.out.println("Branch - Anthony LoPresti");
   }
+
 }
