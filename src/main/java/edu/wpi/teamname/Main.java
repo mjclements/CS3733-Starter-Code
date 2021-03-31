b9080e1b52a4f121fa9f3593ab7656d7c696b29f
duffelmuffinvii <slkbrb@gmail.com>
2021-03-30 22:01:26 -0400

    added my name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -12,6 +12,7 @@ public class Main {
     System.out.println("Team Member Names Below:");
     System.out.println("Michael Clements");
     System.out.println("Team Coach: Michael Clements");
+    System.out.println("Samuel Kwok");
 
   }
 }
