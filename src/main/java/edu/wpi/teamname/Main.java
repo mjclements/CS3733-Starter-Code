835315d228a2a2abf5e35b7f4c391c61f518cd64
Nestor <nlzx432@gmail.com>
2021-03-31 12:40:21 -0400

    Added "Hi Hi" print to main()

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.println("Team Member Names Below:");
     System.out.println("Michael Clements");
     System.out.println("Team Coach: Michael Clements");
-    System.out.println("Hi");
+    System.out.println("Hi Hi");
 
   }
 }
