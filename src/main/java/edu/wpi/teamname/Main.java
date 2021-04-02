6e4cf13a1c4c7a634aa6122304bf652313b3c483
Mason Powell <mvpowell@wpi.edu>
2021-04-01 21:27:58 -0400

    adding name, checking to see if I can push to git

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -14,5 +14,8 @@ public class Main {
     System.out.println("Team Coach: Michael Clements");
     System.out.println("Nathan Rogers");
     System.out.println("Reily Siegel");
+    System.out.println("Mason Powell");
+
+
   }
 }
